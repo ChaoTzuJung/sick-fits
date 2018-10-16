a52655bd4fc370437f8caff2c69c61be5a96f591
ChaoTzuJung <s110319022@stu.ntue.edu.tw>
2018-10-16 21:31:09 +0800

    style: Add styled-component to project

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -2,12 +2,31 @@ import React, { Component } from 'react';
 import Header from './Header';
 import Meta from './Meta';
 
+import styled from 'styled-components';
+
+const MyButton = styled.button`
+    background: red;
+    span {
+        border: 1px solid #fff;
+    }
+    .poop {
+        font: 100px;
+    }
+    font-size:  ${props => (props.huge ? '100px' : '')};
+`
 export default class Page extends Component {
     render() {
         return (
             <div>
                 <Meta />
                 <Header />
+                <MyButton>
+                    Click me
+                    <span className="poop">💩</span>
+                </MyButton>
+                <MyButton huge>
+                    Click me
+                </MyButton>
                 {this.props.children}
             </div>
         )
